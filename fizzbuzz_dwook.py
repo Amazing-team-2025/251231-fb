81c610c69c821722e8ebe522d3f172669f7c4159
dwook <igerapex@gmail.com>
2025-12-31 16:01:19 +0900

    fix: Remove parentheses

--- fizzbuzz_dwook.py
+++ fizzbuzz_dwook.py
@@ -1,9 +1,9 @@
 for i in range(1, 31 +1):
-  if (i % 15 == 0):
+  if i % 15 == 0:
     print("fizzbuzz")
-  elif (i % 3 == 0):
+  elif i % 3 == 0:
     print("fizz")
-  elif (i % 5 == 0):
+  elif i % 5 == 0:
     print("buzz")
   else:
     print(i)
